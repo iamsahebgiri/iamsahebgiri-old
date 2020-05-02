23e7436cb31728dc500af77455c15c6c02034062
saheb Giri <sahebgiri578@gmail.com>
2020-05-02 11:18:55 +0530

    :star: added card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import React from "react"
 import globalStyles from '../styles/global.module.scss'
 import Layout from '../layouts/layout'
 
+import Card from '../components/card'
+
 export default () => (
     <Layout>
-        <h1 className={globalStyles.text}>
+        {/* <h1 className={globalStyles.text}>
             Home Page
-        </h1>
+        </h1> */}
+        <Card />
     </Layout>
 )
