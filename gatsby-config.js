181d527f5255d209bf3a1228bc96ae3788fc9633
saheb Giri <sahebgiri578@gmail.com>
2020-05-02 17:42:03 +0530

    :laugh: added prismjs code highlighing

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,15 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-remark`
+    {
+      resolve :`gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`
+          }]
+      }
+    }
+    
   ]
 }
