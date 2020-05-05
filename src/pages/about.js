742cb93142c143e454fcbe14e2f58926fb190711
saheb Giri <sahebgiri578@gmail.com>
2020-05-05 17:02:43 +0530

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,8 +8,21 @@ export default () => {
     <Layout>
       <div className={globalStyles.whiteContainer}>
         <h1>About Me</h1>
-        <p>I am 18 years old indie developer. I built stuff for fun.</p>
-        <p>I started my journey as a tech enthu and find web more interestring back in the days 2014. </p>
+        <p>I am an 18 years old indie developer or you may call entreprenerd. I am passionate about technology, business and startup. </p>
+        <p>My journey is little different then usual. I started touching computer when I entered class 5, yes thats right. Prior to that I have never seen computer. I had been reading in govt. school and there was no facility of computer back in those days. I had never knew  I/O devices, neither I started LOGO like other student of my class. I started learning HTML in class 5 and urged my parents to buy me a laptop. This was the time when I went to an English medium School, where I learned many things. </p>
+
+        <p>I started my journey as a tech enthu and find web more interesting back in the 2014. Since then I have constanly learning new thing everday. </p>
+        <p>I started reading Java books while I was in class 8. I find the book interesting and build couple of android applications before class 10. Before 10 I have learnt C++ and Java, HTML, CSS and Javascript. There is nothing special about it. It becomes special when I say you that in computer was the subject only upto class 8.</p>
+
+        <h2>
+          Areas of Interest
+        </h2>
+        <ol>
+          <li>Competitive Programming</li>
+          <li>Artificial Intelligence</li>
+          <li>Mathematics</li>
+          <li>UI Design</li>
+        </ol>
       </div>
     </Layout>
   )
